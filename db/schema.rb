ce3a3f8a83c7b04473a3689d7ebd39436a9c6eab
yuto tomita <qualidea01@gmail.com>
2020-02-08 20:45:20 +0900

    ユーザーモデルにユーザー追加した

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_02_08_112518) do
 
   create_table "users", force: :cascade do |t|
-    t.string "userneme"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Yuto", password: "password")
+User.create(username:"Yuika", password: "password")
+User.create(username:"Kentaro", password: "password")
+User.create(username:"Hihumin", password: "password")
+User.create(username:"Tokki-", password: "password")
+User.create(username:"Moririn", password: "password")
+
