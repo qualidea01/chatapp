cd6b46880cad80469737ef3d675aa83e0a34c667
yuto tomita <qualidea01@gmail.com>
2020-02-10 21:41:38 +0900

    ログアウト実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,23 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(username: params[:session] [:username])
+		#もし認証に
+		if user && user.authenticate(params[:session] [:password])
+			#成功したら
+			session[:user_id] = user.id
+			flash[:success] = "ログインに成功しました"
+			redirect_to root_path
+		else
+			#失敗したら
+			flash.now[:error] = "ログインに失敗しました。再度ログインしてください"
+			render 'new'
+		end
 	end
 
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "ログアウトしました"
+		redirect_to login_path
+	end
 end
