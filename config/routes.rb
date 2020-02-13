0f3f4e16f9c6266572274609951d5e9deb2a85aa
yuto tomita <qualidea01@gmail.com>
2020-02-13 21:31:53 +0900

    messages controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  # messagescontrollerのcreateアクションのことを指す
+  post 'message', to: 'messages#create'
 end
