71b23eff47bd1c5a9e2e7842eb7300109ecdf874
yuto tomita <qualidea01@gmail.com>
2020-02-13 20:45:27 +0900

    flash作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,18 +8,18 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session] [:password])
 			#成功したら
 			session[:user_id] = user.id
-			flash[:success] = "ログインに成功しました"
+			flash[:notice] = "ログインに成功しました"
 			redirect_to root_path
 		else
 			#失敗したら
-			flash.now[:error] = "ログインに失敗しました。再度ログインしてください"
+			flash.now[:danger] = "ログインに失敗しました。再度ログインしてください"
 			render 'new'
 		end
 	end
 
 	def destroy
 		session[:user_id] = nil
-		flash[:success] = "ログアウトしました"
+		flash[:notice] = "ログアウトしました"
 		redirect_to login_path
 	end
 end
