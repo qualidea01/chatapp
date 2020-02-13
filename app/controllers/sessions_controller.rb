6d565052ebcde4e807b7ee884bd3f2410c7d9f95
yuto tomita <qualidea01@gmail.com>
2020-02-13 20:56:06 +0900

    flash消える機能追加　＋　ユーザーアクセス制限追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+	before_action :logged_in_redirect, only: [:new, :create]
 	def new 
 	end
 
@@ -21,5 +22,14 @@ class SessionsController < ApplicationController
 		session[:user_id] = nil
 		flash[:notice] = "ログアウトしました"
 		redirect_to login_path
+	end
+
+	private
+
+	def logged_in_redirect
+		if logged_in?
+			flash[:danger] = "既にログインしています"
+			redirect_to root_path
+		end
 	end
 end
